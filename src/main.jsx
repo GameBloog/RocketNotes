19a832c98aec5cde8630f8b75e703579e40ba3a5
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-07-27 12:32:29 -0300

    making new pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client"
 import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./styles/global.js"
 import theme from "./styles/theme.js"
-import { SingIn } from "./pages/SingIn"
+import { Profile } from "./pages/Profile"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SingIn />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
