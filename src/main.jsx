badc5c0f802152d29db496fd1950502bc68d4e56
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-07-27 13:59:49 -0300

    Create New and NewItem

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client"
 import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./styles/global.js"
 import theme from "./styles/theme.js"
-import { Profile } from "./pages/Profile"
+import { New } from "./pages/New"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
