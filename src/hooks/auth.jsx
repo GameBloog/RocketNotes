7c3f638c50e61c10938f7e93fb3f43926be8f593
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-08-02 23:58:45 -0300

    doing "edit user information"

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 import { api } from "../services/api"
 
 export const AuthContext = createContext({})
@@ -11,7 +11,11 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password })
       const { user, token } = response.data
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      localStorage.setItem("@rocketnotes:token", token)
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
       setData({ user, token })
     } catch (error) {
       if (error.response) {
@@ -22,8 +26,53 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    const user = localStorage.removeItem("@rocketnotes:user")
+    const token = localStorage.removeItem("@rocketnotes:token")
+
+    setData({})
+  }
+
+  async function updateProfile({ user }) {
+    try {
+      await api.put("/users", user)
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+
+      setData({ user, token: data.token })
+      alert("Perfil atualizado!")
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Não foi possível atualizar o perfil.")
+      }
+    }
+  }
+
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketnotes:user")
+    const token = localStorage.getItem("@rocketnotes:token")
+
+    if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      })
+    }
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        updateProfile,
+        user: data.user,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react"
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Link } from "react-router-dom"
+import { useAuth } from "../../hooks/auth"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { Avatar, Container, Form } from "./style"
 
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, SetPasswordOld] = useState()
+  const [passwordNew, SetPassworNew] = useState()
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld,
+    }
+
+    await updateProfile({ user })
+  }
+
   return (
     <Container>
       <header>
@@ -22,12 +41,35 @@ export function Profile() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          setEmail
+          onChange={(e) => SetPasswordOld(e.target.value)}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => SetPassworNew(e.target.value)}
+        />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )
