3c44c3efa0f4b9c4f96d620cecaa32c2c2650926
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-08-03 21:55:58 -0300

    creating filter by tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react"
 import { FiPlus } from "react-icons/fi"
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+import { api } from "../../services/api"
 import { Header } from "../../components/Header"
 import { Note } from "../../components/Note"
 import { Input } from "../../components/Input"
@@ -7,6 +9,31 @@ import { Section } from "../../components/Section"
 import { ButtonText } from "../../components/ButtonText"
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+    }else{
+
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return (
     <Container>
       <Brand>
@@ -15,14 +42,22 @@ export function Home() {
       <Header />
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive />
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            isActive={tagsSelected.length === 0}
+          />
         </li>
-        <li>
-          <ButtonText title="React" />
-        </li>
-        <li>
-          <ButtonText title="Nodejs" />
-        </li>
+        {tags &&
+          tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))}
       </Menu>
       <Search>
         <Input placeholder="Pesquisar pelo título" />
